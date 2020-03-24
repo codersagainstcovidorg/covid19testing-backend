e0d018796d006a096c1135243a09f4f8f70b7294
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2020-03-24 15:54:17 -0400

    fix ssm

--- config.py
+++ config.py
@@ -3,14 +3,17 @@ from boto3 import client
 
 APP_NAME = "backend"
 
-ssm_client = client('ssm')
+ssm_client = client('ssm', region_name=getenv("AWS_REGION", 'us-east-1'))
 
 def get_param(param_name, decrypt=True):
-  response = ssm_client.get_parameters_by_path(
-    Path=f'{getenv("ENVIRONMENT", "DEV")}/{APP_NAME}/{param_name}',
-    WithDecryption=decrypt
-  )
-  return response.get("Parameters")[0].get("Value")
+  try:
+    response = ssm_client.get_parameter(
+      Name=f'/{getenv("ENVIRONMENT", "dev")}/{APP_NAME}/{param_name}',
+      WithDecryption=decrypt
+    )
+    return response.get("Parameter").get("Value")
+  except ssm_client.exceptions.ParameterNotFound:
+    return ""
 
 class Config:
     """
@@ -18,9 +21,10 @@ class Config:
     """
 
     # General
-    FLASK_DEBUG = True if "DEV" in getenv("ENVIRONMENT") else False
+    FLASK_DEBUG = True if getenv("ENVIRONMENT") is None else False
     SECRET_KEY = get_param("SECRET_KEY")
 
     # Database
+    # [DB_TYPE]+[DB_CONNECTOR]://[USERNAME]:[PASSWORD]@[HOST]:[PORT]/[DB_NAME]
     SQLALCHEMY_DATABASE_URI = get_param("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False

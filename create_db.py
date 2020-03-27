33bb87464e0959fbe30404e23ab01cb2731d8451
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2020-03-27 14:30:42 -0400

    update create_db function

--- create_db.py
+++ create_db.py
@@ -1,7 +1,26 @@
 from config import Config
+from os import getenv
 import application 
+from boto3 import client
+
+APP_NAME = "backend"
+
+ssm_client = client('ssm', region_name=getenv("AWS_REGION", 'us-east-1'))
+
 
 app = application.create_app()
+
+def get_param(param_name, decrypt=True):
+  try:
+    response = ssm_client.get_parameter(
+      Name=f'/{getenv("ENVIRONMENT", "dev")}/{APP_NAME}/{param_name}',
+      WithDecryption=decrypt
+    )
+    return response.get("Parameter").get("Value")
+  except ssm_client.exceptions.ParameterNotFound:
+    return ""
+
+app.config["SQLALCHEMY_DATABASE_URI"] = get_param("SQLALCHEMY_DATABASE_URI") if getenv("ENVIRONMENT") is not None else getenv("SQLALCHEMY_DATABASE_URI")
 
 with app.app_context():
   from application.database import models
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,4 +8,8 @@ if [[ "$1" == "start" ]]; then
   exec /usr/bin/supervisord
 fi
 
+if [[ "$1" == "create" ]]; then
+  exec python create_db.py
+fi
+
 exec "$@"

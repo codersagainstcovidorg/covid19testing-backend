38a0586c6103b89b58241320956de4ccc1286dcb
Jorge Caballero <jorge.caballero@icloud.com>
2020-05-10 20:20:02 -0700

    Revert "creating table that will hold slugs"
    This reverts commit 4913522320ca801a72f4eda337579728c37ab604.

--- application/database/sql/create_table_slugs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS slugs(
-    record_id         SERIAL                    UNIQUE,
-    slug              text                      NOT NULL PRIMARY KEY,
-    entity_kind       char(24)                  NOT NULL DEFAULT 'location',
-    entity_id         text                      NOT NULL,
-    created_on        timestamp with time zone  NOT NULL DEFAULT now(),
-    updated_on        timestamp with time zone  NOT NULL DEFAULT now(),
-    deleted_on        timestamp with time zone
-);

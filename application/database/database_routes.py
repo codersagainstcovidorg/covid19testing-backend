402a71c19145d6f506e1b8ccd71a9f78f8488dbd
Jorge Caballero <jorge.caballero@icloud.com>
2020-05-10 20:18:27 -0700

    Removing some fields from default set

--- application/database/database_routes.py
+++ application/database/database_routes.py
@@ -78,15 +78,15 @@ def list_location():
 
   # all default fields
   fields_to_return = [
-    'additional_information_for_patients', 'created_on', 'data_source', 'deleted_on', 'geojson', 'is_collecting_samples',
+    'additional_information_for_patients', 'data_source', 'is_collecting_samples',
     'is_collecting_samples_by_appointment_only', 'is_collecting_samples_for_others', 'is_collecting_samples_onsite', 'is_evaluating_symptoms',
-    'is_evaluating_symptoms_by_appointment_only', 'is_hidden', 'is_ordering_tests', 'is_ordering_tests_only_for_those_who_meeting_criteria', 
-    'is_processing_samples', 'is_processing_samples_for_others', 'is_processing_samples_onsite', 'is_verified', 'location_address_locality',
-    'location_address_postal_code', 'location_address_postal_code', 'location_address_region', 'location_address_street', 'location_contact_phone_appointments',
+    'is_evaluating_symptoms_by_appointment_only', 'is_ordering_tests', 'is_ordering_tests_only_for_those_who_meeting_criteria', 
+    'is_processing_samples', 'is_processing_samples_for_others', 'is_processing_samples_onsite', 'location_address_locality',
+    'location_address_postal_code', 'location_address_region', 'location_address_street', 'location_contact_phone_appointments',
     'location_contact_phone_covid', 'location_contact_phone_main', 'location_contact_url_covid_appointments', 'location_contact_url_covid_info',
     'location_contact_url_covid_screening_tool', 'location_contact_url_covid_virtual_visit', 'location_contact_url_main', 'location_hours_of_operation',
-    'location_id', 'external_location_id', 'location_latitude', 'location_longitude', 'location_name', 'location_place_of_service_type',
-    'location_specific_testing_criteria', 'location_status', 'raw_data', 'reference_publisher_of_criteria', 'updated_on', 'record_id'
+    'location_id', 'location_latitude', 'location_longitude', 'location_name', 'location_place_of_service_type',
+    'location_specific_testing_criteria', 'location_status', 'raw_data', 'reference_publisher_of_criteria', 'updated_on', 'created_on'
   ]
 
   # allow caller to request less fields (smaller payload)

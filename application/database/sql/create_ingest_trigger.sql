fa483f6e6c9bc5e9218378aaf44078265f66f5cf
Jorge Caballero <jorge.caballero@icloud.com>
2020-05-20 06:25:45 -0700

    bug fix

--- application/database/sql/create_ingest_trigger.sql
+++ application/database/sql/create_ingest_trigger.sql
@@ -14,17 +14,17 @@ RETURNS TRIGGER
     -- Perform the required operation on data_ingest, and create a row in audit_data_ingest to reflect the change.
     --    
     DECLARE
-        data_source_name TEXT;
+        data_source_name TEXT := 'giscorps';
     BEGIN
       -- Determine which data source triggered action
-      EXECUTE 'SELECT COALESCE(first_value("data_source") OVER (ORDER BY "record_id" DESC), ' || '''giscorps''' || ') FROM data_ingest LIMIT 1' INTO data_source_name;      
+      EXECUTE 'SELECT COALESCE(NULLIF(first_value("data_source") OVER (ORDER BY "record_id" DESC), ' || '''<NULL>'')' || ', ' || '''giscorps''' || ') FROM data_ingest LIMIT 1' INTO data_source_name;      
       
       -- Check that operation is an insert
       IF (TG_OP = 'INSERT') THEN
         RAISE NOTICE '`%` operation on `%` triggered `begin_data_ingest()` for `data_source` == `%`', TG_OP, TG_TABLE_NAME, data_source_name;
-        INSERT INTO audit_data_ingest VALUES(DEFAULT, 'INSERT', 'START', data_source_name, clock_timestamp());        
+        INSERT INTO audit_data_ingest VALUES(DEFAULT, 'INSERT', 'START', COALESCE(data_source_name, 'giscorps'), clock_timestamp());        
         PERFORM ingest_data();
-        INSERT INTO audit_data_ingest VALUES(DEFAULT, 'INSERT', 'COMPLETE', data_source_name, clock_timestamp());
+        INSERT INTO audit_data_ingest VALUES(DEFAULT, 'INSERT', 'COMPLETE', COALESCE(data_source_name, 'giscorps'), clock_timestamp());
         
       END IF;
       RETURN NULL; -- result is ignored since this is an AFTER trigger

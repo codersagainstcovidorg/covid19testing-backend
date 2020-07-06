516587fbde0026b21b01b939c00768724c1b48f9
Jorge Caballero <jorge.caballero@icloud.com>
2020-07-05 22:18:38 -0700

    interval save

--- scripts/python/fetch_data.py
+++ scripts/python/fetch_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import pandas as pd
-from urllib.request import Request, urlopen
+# from urllib.request import Request
 import requests
 import json
 import csv
@@ -15,7 +15,7 @@ timestr = time.strftime("%Y-%m-%dT%H%M") # timestr = time.strftime("%Y-%m-%dT%H%
 
 # GISCorps CSV (full, from url)
 giscorps_raw_csv = {
-    "name": 'GISCorps CSV (from url)',
+    "src_name": 'GISCorps CSV (from url)',
     "src_path": 'https://opendata.arcgis.com/datasets/d7d10caf1cec43e0985cc90fbbcf91cb_0.csv',
     "src_path_type": "url",
     "src_format": 'csv',
@@ -24,12 +24,13 @@ giscorps_raw_csv = {
     "dst_path_type": "file",
     "dst_format": 'csv',
     "does_need_proc": False,
-    "jqstr": None
+    "jqstr": None,
+    "melt_params": None
 }
 
 # GISCorps JSON (full, from url)
 giscorps_raw_json_full = {
-    "name": 'GISCorps JSON (full, from url)',
+    "src_name": 'GISCorps JSON (full, from url)',
     "src_path": 'https://services.arcgis.com/8ZpVMShClf8U8dae/arcgis/rest/services/TestingLocations_public/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json',
     "src_path_type": "url",
     "src_format": 'json',
@@ -38,12 +39,13 @@ giscorps_raw_json_full = {
     "dst_path_type": "file",
     "dst_format": 'json',
     "does_need_proc": False,
-    "jqstr": None
+    "jqstr": None,
+    "melt_params": None
 }
 
 # GISCorps JSON (filtered)
 giscorps_raw_json_filtered = {
-    "name": 'GISCorps JSON (filtered)',
+    "src_name": 'GISCorps JSON (filtered)',
     "src_path": 'https://services.arcgis.com/8ZpVMShClf8U8dae/arcgis/rest/services/TestingLocations_public/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID,facilityid,name,fulladdr,municipality,agency,agencytype,phone,agencyurl,operhours,numvehicles,testcapacity,status,CreationDate,EditDate,drive_through,appt_only,referral_required,services_offered_onsite,call_first,virtual_screening,health_dept_url,State,GlobalID,data_source,county,red_flag,start_date,end_date,type_of_test,test_processing,fine_print&outSR=4326&f=json',
     "src_path_type": "url",
     "src_format": 'json',
@@ -52,12 +54,13 @@ giscorps_raw_json_filtered = {
     "dst_path_type": "file",
     "dst_format": 'json',
     "does_need_proc": False,
-    "jqstr": None
+    "jqstr": None,
+    "melt_params": None
 }
 
 # GISCorps JSONL (filtered) - this makes the line-delimited version
 giscorps_proc_json_filtered = {
-    "name": 'GISCorps JSONL (filtered)',
+    "src_name": 'GISCorps JSONL (filtered)',
     "src_path": 'https://services.arcgis.com/8ZpVMShClf8U8dae/arcgis/rest/services/TestingLocations_public/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID,facilityid,name,fulladdr,municipality,agency,agencytype,phone,agencyurl,operhours,numvehicles,testcapacity,status,CreationDate,EditDate,drive_through,appt_only,referral_required,services_offered_onsite,call_first,virtual_screening,health_dept_url,State,GlobalID,data_source,county,red_flag,start_date,end_date,type_of_test,test_processing,fine_print&outSR=4326&f=json',
     "src_path_type": "url",
     # "src_path": f'{DEST_BASE_GISCORPS}/Raw/json/{timestr}_giscorps_filtered.json',
@@ -68,12 +71,13 @@ giscorps_proc_json_filtered = {
     "dst_path_type": "file",
     "dst_format": 'jsonl',
     "does_need_proc": True,
-    "jqstr": '.features[]'
+    "jqstr": '.features[]',
+    "melt_params": None
 }
 
 # GISCorps CSV for Big Query (filtered)
 giscorps_proc_csv_filtered_bq = {
-    "name": 'GISCorps CSV for Big Query (filtered)',
+    "src_name": 'GISCorps CSV for Big Query (filtered)',
     "src_path": 'https://services.arcgis.com/8ZpVMShClf8U8dae/arcgis/rest/services/TestingLocations_public/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID,facilityid,name,fulladdr,municipality,agency,agencytype,phone,agencyurl,operhours,numvehicles,testcapacity,status,CreationDate,EditDate,drive_through,appt_only,referral_required,services_offered_onsite,call_first,virtual_screening,health_dept_url,State,GlobalID,data_source,county,red_flag,start_date,end_date,type_of_test,test_processing,fine_print&outSR=4326&f=json',
     "src_path_type": "url",
     "src_format": 'json',
@@ -81,11 +85,12 @@ giscorps_proc_csv_filtered_bq = {
     "dst_path_type": "file",
     "dst_format": 'csv',
     "does_need_proc": True,
-    "jqstr": '[(.features[] | (.attributes + .geometry) | .["longitude"] = .x | .["latitude"] = .y | del(.x, .y))]'
+    "jqstr": '[(.features[] | (.attributes + .geometry) | .["longitude"] = .x | .["latitude"] = .y | del(.x, .y))]',
+    "melt_params": None
 }
 
 # [giscorps_raw_csv, giscorps_raw_json_full, giscorps_raw_json_filtered, giscorps_proc_json_filtered, giscorps_proc_csv_filtered_bq]
-url_list = [giscorps_proc_json_filtered]
+url_list = [giscorps_raw_csv]
 
 
 def get_data(src: dict, dst_path_prefix: str = timestr):
@@ -95,7 +100,7 @@ def get_data(src: dict, dst_path_prefix: str = timestr):
                'Referer': 'https://codersagainstcovid.org', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
   
   # Assign source-specific variables
-  name = src["name"]
+  src_name = src["src_name"]
   src_path = src["src_path"]
   src_path_type = src["src_path_type"]
   src_format = src["src_format"]
@@ -105,15 +110,16 @@ def get_data(src: dict, dst_path_prefix: str = timestr):
   dst_format = src["dst_format"]
   does_need_proc = src["does_need_proc"]
   jqstr = src["jqstr"]
+  melt_params = src["melt_params"]
   
   # Open data if source is file
   if src_path_type == 'file':
     with open(src_path, 'rb') as f:
-        print(f'Reading `{name}` from `{src_path}` ...\n')
+        print(f'Reading `{src_name}` from `{src_path}` ...\n')
         f.read()
   # Get data if source is URL
   elif src_path_type == 'url':
-    print(f'Getting `{name}` from `{src_path}` ...\n')
+    print(f'Getting `{src_name}` from `{src_path}` ...\n')
     # Make the request
     src_connector = requests.get(src_path, headers=myHeaders)
     
@@ -123,22 +129,45 @@ def get_data(src: dict, dst_path_prefix: str = timestr):
       
       # Transform data, if necessary
       if does_need_proc == True:
-        dst_data = transform_data(src_data, src_format, dst_format, jqstr) #.all()[0:3]
+        dst_data = transform_data(src_name, src_data, src_format, dst_format, jqstr)  # .all()[0:3]
         
       
       # Write the value to its destination
-      request_write(dst_data, dst_path, dst_path_type, dst_format)
+      request_write(dst_data, dst_path, dst_path_type, dst_format, src_format)
       return
-  # Throw an error otherwise
+    elif src_format == 'csv':
+      src_data = src_connector
+      dst_data = src_data
+        
+      # Transform data, if necessary
+      if does_need_proc == True:
+        dst_data = transform_data(src_name, src_data, src_format, dst_format, jqstr, melt_params)
+        # Write the value to its destination
+        request_write(dst_data, dst_path, dst_path_type, dst_format,src_format)
+      else:
+        dst_data = src_data.content
+        # Write the value to its destination
+        request_write(dst_data, dst_path, dst_path_type, dst_format,src_format)
+        
+        # print(f'Writing to `{dst_path}` ...\n')
+        # open(dst_path, 'wb').write(src_data.content)
+
+        # print(f'SUCCESS | Done writing to `{dst_path}`\n')
+        # return
+      return
+    else:
+      print(f'ERROR | Unknown or unrecognized value for `src_path_type`: {src_path_type}')
+      return
+  # Display error
   else:
     print(f'ERROR | Unknown or unrecognized value for `src_path_type`: {src_path_type}')
     return    
 
 
-def transform_data(src_data, src_format: str, dst_format: str, jqstr: str):
+def transform_data(src_name: str, src_data, src_format: str, dst_format: str, jqstr: str, melt_params: dict={}):
   # Transform data from json -> ???
   if src_format == 'json':
-    # Re-shaping, converting, etc.
+    # Re-shaping, converting, filtering, sorting, etc.
     if dst_format == 'csv':
       print(f'Transforming data: `{src_format}` -> `{dst_format}` ...')
       proc_jqstr = (jqstr if (len(jqstr) > 2) else '.') + \
@@ -148,7 +177,6 @@ def transform_data(src_data, src_format: str, dst_format: str, jqstr: str):
       # Overwrite the variable assignment using the transformed result
       return jq.compile(proc_jqstr).input(src_data)
     
-    # Filtering, sorting, etc.
     elif dst_format.startswith('json'):
       print(f'Transforming data: `{src_format}` -> `{dst_format}` ...')
       proc_jqstr = (jqstr if (len(jqstr) > 2) else '.')
@@ -158,22 +186,60 @@ def transform_data(src_data, src_format: str, dst_format: str, jqstr: str):
     else:
       print(f'ERROR | Unknown or unrecognized transformation: `{src_format}` -> `{dst_format}`')
       return src_data
+  # Transform data from csv -> ???
+  elif src_format == 'csv':
+    # Re-shaping, converting, filtering, sorting, etc.
+    if dst_format == 'csv':
+      if (melt_params.values().count > 0):
+        id_vars = melt_params["id_vars"]
+        var_name = melt_params["var_name"]
+        value_name = melt_params["value_name"]
+        
+        print(f'Transforming data: `{src_format}` -> `{dst_format}` ...')
+        # read the normalized data
+        df = pd.read_csv(src_data)
+
+        # melt the normalized file
+        le = pd.melt(df, id_vars=id_vars, var_name=var_name,
+                    value_name=value_name)
+        
+        # apply filters
+        if (src_name.lower.find('usafacts') != -1):
+          le2 = le[~le.date.str.contains("Unnamed")]
+          le2 = le2[~le2.countyFIPS != 0]
+        else:
+          le2 = le
+        
+        return le2
+      else:
+        print(f'ERROR | Unknown or unrecognized transformation: `{src_format}` -> `{dst_format}`')
+        return src_data
+    # Otherwise return the source data unchanged
+    else:
+      print(f'ERROR | Unknown or unrecognized transformation: `{src_format}` -> `{dst_format}`')
+      return src_data
+    
   # Otherwise return the source data unchanged
   else:
     print(f'ERROR | Unknown or unrecognized transformation: `{src_format}` -> `{dst_format}`')
     return src_data
-  
 
-def request_write(dst_data, dst_path, dst_path_type, dst_format):
+def request_write(dst_data, dst_path: str, dst_path_type: str, dst_format: str, src_format: str=None):
   # Write the value to its destination
-  if dst_path_type == 'file':
+  if dst_path_type == 'file':      
     if dst_format == 'csv':
-      print(f'Writing to `{dst_path}` ...\n')
-      with open(dst_path, 'w', encoding='utf-8', newline='') as f:
-        wr = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
-        wr.writerows(dst_data)
-      print(f'SUCCESS | Done writing to `{dst_path}`\n')
-      return
+      if src_format == 'csv':
+        print(f'Writing to `{dst_path}` ...\n')
+        open(dst_path, 'wb').write(dst_data)
+        print(f'SUCCESS | Done writing to `{dst_path}`\n')
+        return
+      else:
+        print(f'Writing to `{dst_path}` ...\n')
+        with open(dst_path, 'w', encoding='utf-8', newline='') as f:
+          wr = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+          wr.writerows(dst_data)
+        print(f'SUCCESS | Done writing to `{dst_path}`\n')
+        return
     elif dst_format == 'jsonl':
       print(f'Writing to `{dst_path}` ...\n')
       with open(dst_path, 'w', encoding='utf-8', newline='') as f:
